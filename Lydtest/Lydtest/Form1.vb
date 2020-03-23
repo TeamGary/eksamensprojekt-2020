2b575016fa2a09cd6e5677ac730083321be0e32d
Gary Madsen <gm@tradium.dk>
2020-03-23 13:54:07 +0100

    Dag2

---
+++ Lydtest/Lydtest/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        My.Computer.Audio.Play("C:\Users\gm\source\repos\Lydtest\Lydtest\Resources\Bender-03.wav", AudioPlayMode.Background)
+    End Sub
+End Class
